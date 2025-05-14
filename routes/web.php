d8fe57193a24f363af290e6d04e418b54359954d
nisrinakr <nisrin4kr@gmail.com>
2025-05-14 09:21:08 +0700

    pertemuan 11

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Coba;
+//kalo di java import java.io.*;
+
+//kalo di java Route.get()
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/selamat', function () {
+    return view('welcome');
+});
+
+Route::get('halo', function () {
+	return "<h1>Halo, Selamat datang di tutorial laravel www.malasngoding.com</h1>";
+});
+
+Route::get('blog', function () {
+	return view('blog');
+});
+
+Route::get('hello',[Coba::class, 'helloworld']);
+
