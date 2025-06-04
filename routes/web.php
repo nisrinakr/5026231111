a3536ee118a07e8f63daf4ed7bb72da9b2088218
nisrinakr <nisrin4kr@gmail.com>
2025-06-04 08:35:16 +0700

    CRUD LENGKAP

---
+++ routes/web.php
@@ -0,0 +1,108 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Coba;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PegawaiDBController;
+
+//kalo di java import java.io.*;
+
+//kalo di java Route.get()
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/selamat', function () {
+    return view('welcome');
+});
+
+Route::get('halo', function () {
+	return "<h1>Halo, Selamat datang di tutorial laravel www.malasngoding.com</h1>";
+});
+
+Route::get('blog', function () {
+	return view('blog');
+});
+
+Route::get('hello',[Coba::class, 'helloworld']);
+
+Route::get('linktree', function () {
+	return view('tugas-linktree-todolvr');
+});
+
+Route::get('pertama', function () {
+	return view('pertama');
+});
+
+Route::get('frontend', function () {
+	return view('frontend');
+});
+
+//kumpulan link
+Route::get('frontend', function () {
+	return view('frontend');
+});
+
+Route::get('pertama', function () {
+	return view('pertama');
+});
+
+Route::get('bootstrap1', function () {
+	return view('bootstrap1');
+});
+
+Route::get('bootstrap2', function () {
+	return view('bootstrap2');
+});
+
+Route::get('homepage-teknisi', function () {
+	return view('homepage_teknisi');
+});
+
+Route::get('linktree', function () {
+	return view('tugas-linktree-todolvr');
+});
+
+Route::get('js1', function () {
+	return view('js1');
+});
+
+Route::get('js1', function () {
+	return view('js2');
+});
+
+
+Route::get('ets', function () {
+	return view('ets');
+});
+
+Route::get('dosen', [Coba::class,  'index']);
+
+
+Route::get('/formulir', [PegawaiController::class, 'formulir']); //halaman isian
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']); //action form
+
+//route blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
+//route CRUD
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
+
+Route::get('/pegawai/tambah', [PegawaiDBController::class, 'tambah']);
+
+Route::post('/pegawai/store',[PegawaiDBController::class, 'store']);
+
+Route::get('/pegawai/edit/{id}',[PegawaiDBController::class, 'edit']);
+
+Route::post('/pegawai/update',[PegawaiDBController::class, 'update']);
+
+Route::get('/pegawai/hapus/{id}',[PegawaiDBController::class, 'hapus']);
+
+
+
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+
